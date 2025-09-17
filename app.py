dad6b4a89acb13c6ff675f4d80fa9f01a689442e
muyumq <aphiwitsomphet@gmail.com>
2025-09-17 20:03:30 +0700

    ready

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 def welcome():
 	return jsonify({'message': 'Hello, World!'})
 
-@app.route('/plus/<int:n1>/<int:n2>', methods=['GET'])
+@app.route('/plus/<string:n1>/<string:n2>', methods=['GET'])
 def plus(n1, n2):
-    return jsonify({'message': n1 + n2})
+	n1, n2 = int(n1), int(n2)
+	return jsonify({'message': n1 + n2})
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
